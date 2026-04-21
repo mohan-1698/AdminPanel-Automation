dfb3ecfd8491522924c3ad880a0b692dc690c75c
mohan-1698 <mohanvenky201@gmail.com>
2026-04-21 11:04:37 +0530

    Initialized project

---
+++ src/main/java/com/mock/hackathon/AdminPanel/base/BasePage.java
@@ -0,0 +1,13 @@
+package com.mock.hackathon.AdminPanel.base;
+
+/**
+ * Hello world!
+ *
+ */
+public class BasePage 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
